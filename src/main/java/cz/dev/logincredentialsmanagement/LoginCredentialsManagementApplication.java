e8350721fa54cd588b62c215e9391d1ae8bb118a
Matěj <mckillem@tuta.io>
2023-04-12 10:14:08 +0200

    vytvoření projektu, použit docker pro postgres

---
+++ src/main/java/cz/dev/logincredentialsmanagement/LoginCredentialsManagementApplication.java
@@ -0,0 +1,13 @@
+package cz.dev.logincredentialsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginCredentialsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginCredentialsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/dev/logincredentialsmanagement/LoginCredentialsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.dev.logincredentialsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginCredentialsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
