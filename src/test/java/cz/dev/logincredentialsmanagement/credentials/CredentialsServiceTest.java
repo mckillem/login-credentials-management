75a04127dc906b7c2879ccf1be404db35ed7ba13
Matěj <mckillem@tuta.io>
2023-05-14 11:24:05 +0200

    zásek

--- src/test/java/cz/dev/logincredentialsmanagement/credentials/CredentialsServiceTest.java
+++ src/test/java/cz/dev/logincredentialsmanagement/credentials/CredentialsServiceTest.java
@@ -1,20 +1,25 @@
 package cz.dev.logincredentialsmanagement.credentials;
 
 import cz.dev.logincredentialsmanagement.mock.credentials.CredentialsMock;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -57,37 +62,98 @@ class CredentialsServiceTest {
 	}
 
 //	todo: přidá záznam. Pokud nebude id, tak vytvoří nový jinak dle id najde původní záznam addCredentials()
+//	todo: nesouhlasí expected a actual
 	@Test
-	void itShouldAddCredentials() {
+	void addCredentialsWithoutId() {
 		// Given
+		Credentials one = new Credentials(
+				null,
+				"url",
+				"email",
+				"password",
+				LocalDateTime.now(),
+				null
+		);
+
+		assertThat(one, is(not(nullValue())));
+		
 		// When
+		credentialsService.addCredentials(one);
+		
 		// Then
+		ArgumentCaptor<Credentials> credentialsArgumentCaptor = ArgumentCaptor.forClass(Credentials.class);
+
+		verify(credentialsRepository).save(credentialsArgumentCaptor.capture());
+
+		Credentials capturedCredentials = credentialsArgumentCaptor.getValue();
+
+		AssertionsForClassTypes.assertThat(capturedCredentials).isEqualTo(one);
+	}
+
+//	todo: nesouhlasí expected a actual
+	@Test
+	void addCredentialsWithId() {
+		// Given
+		Credentials one = new Credentials(
+				UUID.randomUUID(),
+				"url",
+				"email",
+				"password",
+				LocalDateTime.now(),
+				null
+		);
+
+		assertThat(one, is(not(nullValue())));
+
+		// When
+		credentialsService.addCredentials(one);
+
+		// Then
+		ArgumentCaptor<Credentials> credentialsArgumentCaptor = ArgumentCaptor.forClass(Credentials.class);
+
+		verify(credentialsRepository).save(credentialsArgumentCaptor.capture());
+
+		Credentials capturedCredentials = credentialsArgumentCaptor.getValue();
+
+		AssertionsForClassTypes.assertThat(capturedCredentials).isEqualTo(one);
 	}
 
 //	todo: zobrazí archivovaný záznam dle getCredentialsArchive()
-//	@Test
-//	void itShouldGetCredentialsArchive() {
-//		// given
-//		UUID id = UUID.randomUUID();
-//		UUID archiveConnectionId = UUID.randomUUID();
-//		Credentials credentials = new Credentials (
-//				id,
-//				"www.tam.tu",
-//				"email",
-//				"pass",
-//				LocalDateTime.now(),
-//				archiveConnectionId
-//		);
+//	todo: nevím jak vyřešit
+	@Test
+	void getCredentialsArchive() {
+		// given
+//		List<Credentials> all = CredentialsMock.getAll();
 //
-////		credentialsRepository.save(credentials);
+//		when(credentialsRepository.findByArchiveConnectionId(CredentialsMock.getAll().stream().filter()));
 //
-//		given(credentialsService.getCredentialsArchive(id)).willReturn(true);
+//		// When
+//		List<Credentials> foundCredentials = credentialsService.getAllCredentials();
 //
-//		// when
-////		snažím se z db načíst záznam, který tam není?
-//		List<Credentials> expected = credentialsService.findByArchiveConnectionId(archiveConnectionId);
-//
-//		// then
-//		assertThat(archiveConnectionId).isEqualTo(expected);
-//	}
+//		// Then
+//		assertThat(foundCredentials, is(not(nullValue())));
+//		verify(credentialsRepository).findAllWhereArchiveConnectionIdIsNull();
+
+		UUID id = UUID.randomUUID();
+		UUID archiveConnectionId = UUID.randomUUID();
+		Credentials credentials = new Credentials (
+				id,
+				"www.tam.tu",
+				"email",
+				"pass",
+				LocalDateTime.now(),
+				archiveConnectionId
+		);
+
+		credentialsRepository.save(credentials);
+
+		given(credentialsService.getCredentialsArchive(id)).willReturn(true);
+
+		// when
+//		snažím se z db načíst záznam, který tam není?
+		List<Credentials> expected = credentialsService.findByArchiveConnectionId(archiveConnectionId);
+
+		// then
+		assertThat(archiveConnectionId).isEqualTo(expected);
+	}
 }
